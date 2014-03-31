fb908ae889194d385c05f2e56277ec3a07ed7cea
yamamoto_junya <yamamoto_junya@cyberagent.co.jp>
2014-03-31 23:10:57 +0900

    add file

---
+++ hook.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import web
+import json
+import hipchat
+
+
+urls = ('/.*','hooks')
+app = web.application(urls, globals())
+
+# hipchat data
+HIP_CHAT_TOKEN = ''
+ROOM_ID  = 
+
+class hooks:
+    def POST(self):
+        data = json.loads(web.data())
+        try:
+            # github v3 json
+            message = data['comment']['_links']['pull_request']['href']
+            self.send(message)
+
+        except:
+            try:
+                message = data['comment']['html_url']
+                self.send(message)
+            except:
+                pass
+
+        return 'OK'
+
+    def send(self, message):
+        hipster = hipchat.HipChat(token = HIP_CHAT_TOKEN)
+        hipster.method('rooms/message',  method = 'POST',  parameters = {'room_id': ROOM_ID, 'from': 'github',  'message': 'Pull Request diff commented on.' + message})
+
+if __name__ == '__main__':
+    app.run()
